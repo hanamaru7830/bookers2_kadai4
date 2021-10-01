d7c547113143ccf5b7ee3fac6365d058030c5f28
hanamaru7830 <xwsmilexw@gmail.com>
2021-10-01 14:10:34 +0000

    課題５終了

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   root :to => "homes#top"
   get "home/about" => "homes#about"
+  
+  get "/search" => "search#search"
 
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
